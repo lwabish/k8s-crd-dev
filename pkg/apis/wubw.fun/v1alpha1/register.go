90063d1cdcd0ef0a4f076bc448ac0f005b099898
lwabish <wubw@pku.edu.cn>
2022-10-26 21:26:38 +0800

    feat: complete register.go

--- pkg/apis/wubw.fun/v1alpha1/register.go
+++ pkg/apis/wubw.fun/v1alpha1/register.go
@@ -1,3 +1,35 @@
 package v1alpha1
 
-const GroupName = "wubw.fun"
+import (
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+const (
+	GroupName    = "wubw.fun"
+	GroupVersion = "v1alpha1"
+)
+
+var (
+	SchemeBuilder      runtime.SchemeBuilder
+	localSchemeBuilder = &SchemeBuilder
+	AddToScheme        = localSchemeBuilder.AddToScheme
+)
+
+func init() {
+	localSchemeBuilder.Register(addKnownTypes)
+}
+
+var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: GroupVersion}
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&Demo{},
+		&DemoList{})
+	metaV1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
